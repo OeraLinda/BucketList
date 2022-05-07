4a7f4469625d658e5accb99c79be5faecd92a426
Yasin <yasinerdemli35@icloud.com>
2022-05-07 15:22:44 +0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Yasin Erdemli on 7.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
